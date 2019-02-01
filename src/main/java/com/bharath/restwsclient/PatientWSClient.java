28b1531295031ae3c9fc0718c7166ec2422fd49d
kongaman <christian.kranz@gmail.com>
2019-02-01 10:45:52 +0100

    dynamically insert uri and parameter

--- src/main/java/com/bharath/restwsclient/PatientWSClient.java
+++ src/main/java/com/bharath/restwsclient/PatientWSClient.java
@@ -4,16 +4,18 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 
 import com.bharath.restwsclient.model.Patient;
 
 public class PatientWSClient {
 	
+	private static final String PATIENT_SERVICE_URL = "http://localhost:8080/restws/services/patientservice";
+
 	public static void main(String[] args) {
 		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/restws/services/patientservice/patients/123");
+		WebTarget target = client.target(PATIENT_SERVICE_URL).path("/patients").path("/{id}").resolveTemplate("id", 123);
+		
 		Builder request = target.request();
 		Patient patient = request.get(Patient.class);
 		
